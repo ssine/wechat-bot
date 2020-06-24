c6aae7a0bce89059ba55fdbac8bb0462cc107369
sine <963366202@qq.com>
2020-06-24 09:26:03 +0800

    add contest puncher

--- src/exercise-puncher.ts
+++ src/exercise-puncher.ts
@@ -10,6 +10,7 @@ type ExercisePuncherConfig = {
 
 type ExercisePuncherData = {
   infos: ExercisePunchInfo[]
+  contests: ContestPunchInfo[]
 }
 
 class ExercisePunchInfo {
@@ -18,6 +19,15 @@ class ExercisePunchInfo {
   time: moment.Moment = moment()
   num: number = 0
   recommend: string = ''
+  comment: string = ''
+}
+
+class ContestPunchInfo {
+  name: string = ''
+  id: string = ''
+  time: moment.Moment = moment()
+  num: number = 0
+  percentile: number = 0
   comment: string = ''
 }
 
@@ -36,7 +46,8 @@ class ExercisePuncher {
     this.bot = bot
     this.config = config
     this.data = {
-      infos: []
+      infos: [],
+      contests: []
     }
     this.inProcess = new Set()
   }
@@ -52,10 +63,10 @@ class ExercisePuncher {
       if (msg.text() === '打卡') {
         this.punchExercise(msg.from(), date)
       } else if (msg.text() === '周赛') {
-
+        this.punchContest(msg.from(), date)
       } else if (msg.text() === '帮助') {
         await this.room.say(
-`欢迎使用打卡 bot ，支持的指令：
+          `欢迎使用打卡 bot ，支持的指令：
 打卡： 进行每日打卡
 周赛： 功能开发中
 帮助： 显示本条帮助`)
@@ -63,7 +74,7 @@ class ExercisePuncher {
     })
 
     new CronJob('0 59 23 * * *', () => {
-    // new CronJob('0 45 2 * * *', () => {
+      // new CronJob('0 45 2 * * *', () => {
       this.dailyReport()
     }, null, true, 'Asia/Shanghai');
   }
@@ -135,6 +146,81 @@ class ExercisePuncher {
     this.inProcess.delete(contact.id)
   }
 
+  async punchContest(contact: Contact, date: Date) {
+    if (this.inProcess.has(contact.id)) {
+      await this.room.say(`[${contact.name()}] 已在打卡对话中。`)
+      return
+    }
+    this.inProcess.add(contact.id)
+    const waitMsg = async () => {
+      const reply = await this.bot.waitForMessage({ room: this.room.id, contact: contact.id })
+      if (reply.text() === '取消') {
+        throw new Error('cancel')
+      }
+      return reply
+    }
+
+    let info = new ContestPunchInfo()
+    info.time = moment(date)
+    const alias = await this.room.alias(contact)
+    const name = alias ? alias : contact.name()
+    info.name = name
+    info.id = contact.id
+
+    if (!this.data.contests) this.data.contests = []
+    let previous = this.data.contests.filter((info) => info.id == contact.id)
+    if (previous.length > 0 && isSameDay(previous[previous.length - 1].time, info.time)) {
+      await this.room.say(`[${name}] 今天已经记录过周赛啦。`)
+      return
+    }
+
+    await this.room.say(`[${name}] 开始记录周赛。 请输入 AC 个数：`)
+    try {
+      while (true) {
+        const reply = await waitMsg()
+        const exerciseNumber = parseInt(reply.text())
+        if (isNaN(exerciseNumber) || exerciseNumber <= 0) {
+          await this.room.say(`输入数字无效。 请输入正整数：`)
+        } else {
+          info.num = exerciseNumber
+          break
+        }
+      }
+
+      await this.room.say(`请输入排名百分位数， 0 - 1 之间， 1 表示排名最高：`)
+      while (true) {
+        const reply = await waitMsg()
+        const percentile = parseFloat(reply.text())
+        if (isNaN(percentile) || percentile < 0 || percentile > 1) {
+          await this.room.say(`输入数字无效。 请输入 0 - 1 之间的浮点数：`)
+        } else {
+          info.percentile = percentile
+          if (percentile > 0.97) {
+            await this.room.say(`大佬受我一拜_(:з」∠)_`)
+          }
+          break
+        }
+      }
+
+      await this.room.say(`请输入备注，无备注请回复 n 。`)
+      let reply = await waitMsg()
+      let commentResponse = '无备注。'
+      if (reply.text().trim().toLocaleLowerCase() !== 'n') {
+        info.comment = reply.text()
+        commentResponse = '备注已记录。'
+      }
+      await this.room.say(commentResponse)
+
+      this.data.contests.push(info)
+      await this.saveData()
+      await this.room.say(`[${name}] 周赛内容已记录，感谢使用^_^`)
+
+    } catch (err) {
+      await this.room.say(`[${name}] 周赛记录取消。`)
+    }
+    this.inProcess.delete(contact.id)
+  }
+
   async saveData() {
     await fs.promises.writeFile(this.config.fileName, JSON.stringify(this.data, null, 2))
     return
@@ -152,7 +238,8 @@ class ExercisePuncher {
       console.log(err)
       console.log('failed to load data, init with empty.')
       this.data = {
-        infos: []
+        infos: [],
+        contests: []
       }
     }
   }
